3839d69f9e49c5495612524a39258f0b60d115e0
mrdoomus <mrdoom.official@gmail.com>
2019-06-20 22:51:55 +0000

    Added 100 and 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number1, number2, number3;
+
+	for (number1 = 0; number1 < 10; number1++)
+	{
+	for (number2 = 0; number2 < 10; number2++)
+	{
+	for (number3 = 0; number3 < 10; number3++)
+	{
+	if (number1 == number2 || number1 == number3 || number2 == number3 || number1 > number2 || number2 > number3)
+	continue;
+	else
+	putchar ((number1 % 10) + '0');
+	putchar ((number2 % 10) + '0');
+	putchar ((number3 % 10) + '0');
+	if (number1 == 7 && number2 == 8 && number3 == 9)
+	{
+	}	
+	else
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}

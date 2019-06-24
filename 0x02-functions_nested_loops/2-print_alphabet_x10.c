849bff3ec6d2e611f981edfa0160c23bbd4f7883
mrdoomus <mrdoom.official@gmail.com>
2019-06-24 21:57:54 +0000

    Added folder with several scripts

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int count;
+for (count = 0; count <= 10; count++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}

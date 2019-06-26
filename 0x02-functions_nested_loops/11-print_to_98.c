2bec8b1f9be6ee54b2db7ce49915be6a49cf3812
mrdoomus <mrdoom.official@gmail.com>
2019-06-26 03:42:41 +0000

    Fixed 11 script

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,64 +12,36 @@ void print_to_98(int n)
 {
 int c;
 
-if (n == 0)
+if (n == 0 || n == 98)
+{
+if (n == 98)
+printf("98\n");
+else
 {
 for (c = n; c <= 98; c++)
 {
-if (c == 98)
-printf("%d", c);
-else
 printf("%d, ", c);
-}
-printf("\n");
-}
-
-if (n == 98)
-{
-for (c = n; c <= n; c++)
-{
 if (c == 98)
-printf("%d", c);
-else
-printf("%d, ", c);
+printf("%d\n", c);
 }
-printf("\n");
 }
-
+}
 if (n > 98)
 {
 for (c = n; c >= 98; c--)
 {
-if (c == 98)
-printf("%d", c);
-else
 printf("%d, ", c);
+if (c == 98)
+printf("%d\n", c);
 }
-printf("\n");
 }
-
-if (n == 81)
+if (n < 98 && n != 0)
 {
 for (c = n; c <= 98; c++)
 {
-if (c == 98)
-printf("%d", c);
-else
 printf("%d, ", c);
-}
-printf("\n");
-}
-
-if (n < 0)
-{
-for (c = n; c <= 98; c++)
-{
 if (c == 98)
-printf("%d", c);
-else
-printf("%d, ", c);
+printf("%d\n", c);
 }
-printf("\n");
 }
-
 }

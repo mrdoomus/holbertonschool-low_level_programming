c120324b1f349d5252f23c52ebe7dc2dd4129593
mrdoomus <mrdoom.official@gmail.com>
2019-06-27 20:43:30 +0000

    Fixed 9 script

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,14 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
+	if (a % 3 == 0 && a % 5 == 0)
+	printf("FizzBuzz ");
 	if (a % 3 == 0)
 	printf("Fizz ");
 	else if (a % 5 == 0)
 	printf("Buzz ");
 	else if (a == 100)
 	printf("Buzz");
-	else if (a % 3 == 0 || a % 5 == 0)
-	printf("FizzBuzz ");
 	else
 	printf("%d ", a);
 	}

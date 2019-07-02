f62ca467c1bd7f3e053e73b56780b968be0e4cd7
mrdoomus <mrdoom.official@gmail.com>
2019-07-02 18:38:38 +0000

    fixed 7th script

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,11 +15,11 @@ int i, j;
 	while (*(str + i) != '\0')
 	i++;
 
-	if (i % 2 !=0)
-		for (j = ((i - 1) / 2); j <= i; j++)
+	if (i % 2 != 0)
+		for (j = ((i + 1) / 2); *(str + j) != '\0'; j++)
 			_putchar(*(str + j));
-	else
-		for (j = (i / 2); j <= i; j++)
+	else if (i % 2 == 0)
+		for (j = (i / 2); *(str + j) != '\0'; j++)
 			_putchar(*(str + j));
 _putchar('\n');
 }

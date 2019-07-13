0f1402daa388e3097870b1c2a9044c0dd2ccbe65
mrdoomus <mrdoom.official@gmail.com>
2019-07-13 18:24:45 +0000

    Fixed scripts 2 3 and 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,26 +15,15 @@ int main(int argc, char *argv[])
 
 int sum = 0;
 
-
-
 if (argc == 3)
 	{
-	if ((*argv[(argc - argc) + 1] >= 48 && *argv[(argc - argc) + 1] <= 57)
-		&& (*argv[(argc - argc) + 2] >= 48 && *argv[(argc - argc) + 2] <= 57))
-		{
-		sum += (atoi(argv[(argc - argc) + 1]) * atoi(argv[(argc - argc) + 2]));
-		printf("%d\n", sum);
-		return (0);
-		}
-	else
-		{
-		printf("Error\n");
-		return (1);
-		}
+	sum += (atoi(argv[(argc - argc) + 1]) * atoi(argv[(argc - argc) + 2]));
+	printf("%d\n", sum);
+	return (0);
 	}
 else
-{
+	{
 	printf("Error\n");
 	return (1);
-}
+	}
 }

b140621eb6012f72f5be60f91c7365ebf7315338
mrdoomus <mrdoom.official@gmail.com>
2019-07-14 17:37:59 +0000

    III fix on 100th script

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,38 +18,50 @@ int i = 0, j = 0, k = 0, l = 0, m = 0, sum = 0, cents = atoi(argv[1]);
 		if (cents < 0)
 			printf("0\n");
 		else
+		{
 			if (cents >= 25)
 			{
 				while (cents >= 25)
+				{
 					cents -= 25;
 					i++;
+				}
 			}
 			if (cents >= 10)
 			{
 				while (cents >= 10)
+				{
 					cents -= 10;
 					j++;
+				}
 			}
 			if (cents >= 5)
 			{
 				while (cents >= 5)
+				{
 					cents -= 5;
 					k++;
+				}
 			}
 			if (cents >= 2)
 			{
 				while (cents >= 2)
+				{
 					cents -= 2;
 					l++;
+				}
 			}
 			if (cents >= 1)
 			{
 				while (cents >= 1)
+				{
 					cents -= 1;
 					m++;
+				}
 			}
 			sum = i + j + k + l + m;
 			printf("%d\n", sum);
+		}
 	}
 	else
 		printf("Error\n");

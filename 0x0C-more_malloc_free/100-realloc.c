e781e8da0f55b6f2abca470a5f3f9ec4a64d60fc
mrdoomus <mrdoom.official@gmail.com>
2019-07-18 22:32:23 +0000

    II Fix on 100th script

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 unsigned int i;
 void *new_ptr;
-char *pnew_ptr, *pold_ptr;
 
 if (ptr == NULL)
 {
@@ -34,16 +33,23 @@ if (new_size == 0 && ptr != NULL)
 if (new_size == old_size)
 	return (ptr);
 
-if (new_size > old_size)
-{
-	new_ptr = malloc(new_size);
+
+new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-	pnew_ptr = (char *)new_ptr;
-	pold_ptr = (char *)ptr;
+
+if (new_size > old_size)
+{
 	for (i = 0; i < old_size; i++)
-		pnew_ptr[i] = pold_ptr[i];
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+}
+else
+{
+	for (i = 0; i < new_size; i++)
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	
 }
 
-return (pnew_ptr);
+free (ptr);
+return (new_ptr);
 }

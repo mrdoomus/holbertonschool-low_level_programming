fd7ebdc814faeda644562d1a5ae24d11dbba67f6
mrdoomus <mrdoom.official@gmail.com>
2019-07-18 22:48:16 +0000

    IIIFeix on 100th script

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,13 +17,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 unsigned int i;
 void *new_ptr;
 
-if (ptr == NULL)
-{
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-		return (NULL);
-}
-
 if (new_size == 0 && ptr != NULL)
 {
 	free(ptr);
@@ -33,10 +26,12 @@ if (new_size == 0 && ptr != NULL)
 if (new_size == old_size)
 	return (ptr);
 
-
 new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-		return (NULL);
+if (new_ptr == NULL)
+	return (NULL);
+
+if (ptr == NULL)
+	return (new_ptr);
 
 if (new_size > old_size)
 {
@@ -47,9 +42,10 @@ else
 {
 	for (i = 0; i < new_size; i++)
 		((char *)new_ptr)[i] = ((char *)ptr)[i];
-	
+
+	free(ptr);
 }
 
-free (ptr);
+free(ptr);
 return (new_ptr);
 }

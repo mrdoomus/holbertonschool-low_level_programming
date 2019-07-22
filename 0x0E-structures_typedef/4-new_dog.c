f1ea2d1f7f05a3ef4af431626555668c8b88658b
mrdoomus <mrdoom.official@gmail.com>
2019-07-22 23:21:38 +0000

    I fix for 3rd and 2nd script, VI fix for 4th script

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@ int _strlen(char *s)
 {
 int i;
 
-while (*(s + i) != '\0')
+for (i = 0; *s != '\0'; s++)
 {
 	i++;
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
+/**
+ * struct dog - Dog data.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Name of the owner.
+ * Description: A structure with the name, age and owner of a dog.
+**/
 typedef struct dog
 {
 	char *name;

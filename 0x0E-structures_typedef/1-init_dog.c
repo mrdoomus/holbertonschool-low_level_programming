8c1c517b0d13b11f1e4b8fdf9c95ed983bd32e65
mrdoomus <mrdoom.official@gmail.com>
2019-07-22 22:56:01 +0000

    Added every script

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog -  Initializes a variable type
+ *
+ * @d: Pointer to struct
+ * @name: Parameter of name
+ * @age: Parameter of age
+ * @owner: Parameter of owner
+ *
+ * Return: No return.
+**/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - Swaps integers wih pointers.
+ *
+ * @s: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+int i;
+
+while (*(s + i) != '\0')
+{
+i++;
+}
+
+return (i);
+}
+
+/**
+ * _strcpy - Swaps integers wih pointers.
+ *
+ * @dest: is a pointer to a char
+ *
+ * @src: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+char buff[1000];
+int i = 0, j, pos = 0;
+
+while (*(src + i) != '\0')
+{
+	buff[i] = *(src + i);
+	i++;
+}
+
+for (j = 0; *(src + j) != '\0' ; j++)
+{
+	*(dest + pos) = buff[j];
+	pos++;
+}
+dest[j] = '\0';
+return (dest);
+}
+
+/**
+ * 
+ *
+ * @name:
+ * @age:
+ * @owner:
+ *
+ * Return:
+**/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+char *newName, *newOwner;
+dog_t *newDog;
+
+newDog = malloc(sizeof(struct dog));
+
+if (newDog == NULL)
+	return(NULL);
+
+newName = malloc(_strlen(name) + 1);
+if (newName == NULL)
+{
+	free(newName);
+	return(NULL);
+}
+
+newOwner = malloc(_strlen(owner) + 1);
+if (newOwner == NULL)
+{
+	free(newOwner);
+	free(newName);
+	return(NULL);
+}
+
+newName = _strcpy(newName, name);
+newOwner = _strcpy(newOwner, owner);
+
+newDog->name = newName;
+newDog->age = age;
+newDog->owner = newOwner;
+
+return(newDog);
+}

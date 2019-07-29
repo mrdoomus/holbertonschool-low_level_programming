0d8ae12d8258b9336b8171d25cba1d9b5988b926
mrdoomus <mrdoom.official@gmail.com>
2019-07-29 02:58:27 +0000

    Added 100th script with assembly code

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,21 @@
+%define sys_write 1
+%define stdout 1
+
+%define sys_exit 60
+%define success 0
+
+%define nl 10
+
+section .data
+
+	message db "Hello, Holberton", nl
+
+section .text
+
+global _start
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall

02341e0a2bc615fc3b2f0cb6f4195e3034239b5d
mrdoomus <mrdoom.official@gmail.com>
2019-08-06 21:35:02 +0000

    Final Fix on 9nth script

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,10 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
-	{
-		newNode = add_nodeint(head, n);
-		return (newNode);
-	}
+		return (add_nodeint(head, n));
 
 	while (*head != NULL)
 	{

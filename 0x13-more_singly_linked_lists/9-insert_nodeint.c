b1b5d700b437e4307fb4a71d1acc0c7a0389dd5c
mrdoomus <mrdoom.official@gmail.com>
2019-08-06 19:45:29 +0000

    Got next of head out of else condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -53,8 +53,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			*head = newNode;
 			return (newNode);
 		}
-		else
-			head = &(*head)->next;
+	head = &(*head)->next;
 	i++;
 	}
 	return (NULL);

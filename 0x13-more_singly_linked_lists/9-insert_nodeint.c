0667baf08dbac82f015013945748e6de76f4b5f4
mrdoomus <mrdoom.official@gmail.com>
2019-08-06 20:22:08 +0000

    Deleted head condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,9 +34,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode;
 	unsigned int i = 0;
 
-	if (head == NULL)
-		return (NULL);
-
 	if (idx == 0)
 	{
 		newNode = add_nodeint(head, n);

cb06f8398ceee20b75069dc96817bee3ae374963
mrdoomus <mrdoom.official@gmail.com>
2019-08-06 20:00:07 +0000

    Added function to pop the head node, changed return values

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,6 +3,27 @@
 #include "lists.h"
 
 /**
+ * pop_listint - Deletes the head node and returns the head node's data
+ * @head: pointer to a struc
+ * Return: Returns nothing
+**/
+int pop_listint(listint_t **head)
+{
+	int i = 0;
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	i = tmp->n;
+	*head = (*head)->next;
+
+	free(tmp);
+	return (i);
+}
+
+/**
  * delete_nodeint_at_index - Deletes a node in a specific position
  * @head: Pointer to a pointer pointing to a struct
  * @index: Index of the list where the new node should be deleted
@@ -18,9 +39,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
+	/*{
 		*head = tmp->next;
 		free(tmp);
 		return (1);
+	}*/
+	pop_listint(head);
+	return (1);
 	}
 
 	for (i = 0; tmp != NULL && i < index; i++)
@@ -29,7 +54,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (tmp == NULL || tmp->next == NULL)
 		return (-1);
 
-	nextNode = tmp->next->next;
+	nextNode = (tmp->next)->next;
 	free(tmp->next);
 	tmp->next = nextNode;
 	return (1);

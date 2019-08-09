e05407802f2a5087f4ca3afcb92ea0c146886941
mrdoomus <mrdoom.official@gmail.com>
2019-08-09 03:51:30 +0000

    Fixed script 1, changed to recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,21 +7,7 @@
 **/
 void print_binary(unsigned long int n)
 {
-	int i = 0;
-
-	if (n == 0)
-		_putchar('0');
-
-	while (n >> i)
-		i++;
-
-	for (i = i - 1; i >= 0; i--)
-	{
-		int num = n >> i;
-
-		if (num & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
+	if (n >> 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }

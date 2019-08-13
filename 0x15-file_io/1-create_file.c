da8fde5e59df3889a1103de29967c344438e2465
mrdoomus <mrdoom.official@gmail.com>
2019-08-13 22:10:32 +0000

    Fixed: Returns 1 when no text is foun but the file is created

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,10 @@ int create_file(const char *filename, char *text_content)
 	int fd = 0, len = 0;
 
 	if (text_content == NULL)
+	{
 		fd = open(filename, O_CREAT);
+		return (1);
+	}
 
 	if (filename == NULL)
 		return (-1);

6014f55dc461e6ee46ee6b05e77e4645fdf7137e
mrdoomus <mrdoom.official@gmail.com>
2019-08-14 04:19:08 +0000

    Fixed: Betty on script 2, separated functions for errors on script 3 and also added while when read > buf_size

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -35,7 +35,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 		else
 		{
-		  	close(fd);
+			close(fd);
 			return (-1);
 		}
 	}
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,44 +2,83 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * error97 - Displays syntaxis error message
+**/
+void error97(void)
+{
+	dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+/**
+ * error98 - Displays can't read error message
+ * @name: Pointer to de name of the unredeable file
+**/
+void error98(char *name)
+{
+	dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", name);
+	exit(98);
+}
+
+/**
+ * error99 - Displays can't write error message
+ * @name: Pointer to de name of the unwritable file
+**/
+void error99(char *name)
+{
+	dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", name);
+	exit(99);
+}
+
+/**
+ * error100 - Displays can't close error message
+ * @value: Pointer to de name of the uncloseable file
+**/
+void error100(int value)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", value);
+	exit(100);
+}
+
+
+/**
+ * main - Function to copy a file into another one
+ * @argc: Amount of arguments
+ * @argv: Value of the arguments
  * Return: Always 0.
 **/
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	int fd = 0, fd2 = 0, len = 0;
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error97();
 	if (argv[1] == NULL)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
+		error98(argv[1]);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		exit(98);
-
+		error98(argv[1]);
 	fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		error99(argv[2]);
 	len = read(fd, buf, 1024);
 	len = write(fd2, buf, len);
-	if (fd2 == -1)
+	if (len == -1)
+		error99(argv[2]);
+	while (len == 1024)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		len = read(fd, buf, 1024);
+		if (len == -1)
+			error98(argv[1]);
+		len = write(fd2, buf, len);
+		if (len == -1)
+			error99(argv[2]);
 	}
+	if (close(fd) == -1)
+		error100(fd);
 
-	close(fd);
-	close(fd2);
+	if (close(fd2) == -1)
+		error100(fd2);
 	return (0);
 }

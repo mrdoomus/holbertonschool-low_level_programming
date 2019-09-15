f51eaa3e9d4b330f7b8afb5a8032aa7ef0416d09
mrdoomus <mrdoom.official@gmail.com>
2019-09-15 03:38:01 +0000

    Added if node is NULL condition

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,7 +15,11 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 
 	for (i = 0; i < index; i++)
+	{
 		node = node->next;
+		if (node == NULL)
+			return (NULL);
+	}
 
 	return (node);
 }

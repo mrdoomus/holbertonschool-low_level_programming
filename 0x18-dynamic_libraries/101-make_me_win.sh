5a9fbaf74d13a756054b7d0f016be51815dbf618
mrdoomus <mrdoom.official@gmail.com>
2019-09-16 21:07:58 +0000

    Updates .sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/mrdoomus/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/hack/libhack.so
-LD_PRELOAD=$PWD/libhack.so
+wget -P ../ https://github.com/mrdoomus/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/hack/libhack.so
+export LD_PRELOAD=../libhack.so

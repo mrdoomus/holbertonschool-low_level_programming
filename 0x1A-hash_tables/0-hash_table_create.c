01d5efabccf84f32e937b808656138a6195cb49f
Holberton <holberton@iMac-de-Holberton.local>
2019-10-11 14:22:37 -0500

    Fixed betty issues with 0 script and added check if key is dup to 3rd script

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,6 +5,7 @@
  * @size: the size of the array
  * Return: pointer to the newly created hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht = NULL;
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t - Adds an element to the hash table
+ * hash_table_set - Adds an element to the hash table
  * @ht: Hash table being set
  * @key: Is the key, can't be empty
  * @value: Value associated with the key, must be duplicated, can be empty
@@ -10,14 +10,13 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
+	hash_node_t *node, *tmp;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	unsigned long int index = key_index((unsigned char *)key, ht->size);
 
-	/*If array in that index is empty, then add the node as head*/
 	if (ht->array[index] == NULL)
 	{
 		node = malloc(sizeof(hash_node_t));
@@ -28,10 +27,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node->next = NULL;
 		ht->array[index] = node;
 	}
-	/*If the array in that index is full,*/
-       /*then point the node to head and then move head to node*/
 	else
 	{
+		tmp = ht->array[index];
+		while (tmp != NULL)
+		{
+			if (!strcmp(ht->array[index]->key, key))
+			{
+				free(ht->array[index]->value);
+				ht->array[index]->value = strdup(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
+
 		node = malloc(sizeof(hash_node_t));
 		if (node == NULL)
 			return (0);
@@ -40,6 +49,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node->next = ht->array[index];
 		ht->array[index] = node;
 	}
-
 	return (1);
 }

0ef90991209d6a01214ae8cfc4df2de8c5924694
mrdoomus <holberton@iMac-de-Holberton.local>
2019-10-28 15:58:26 -0500

    master: Added 100-shell_sort C.F Sorts integers using Shell sort algorithm.

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -26,4 +26,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void partition_caller(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
+void shell_sort(int *array, size_t size);
 #endif

0f42855c41ea20d7b41176624321b9d2118c2b41
mrdoomus <holberton@iMac-de-Holberton.local>
2019-10-29 10:50:25 -0500

    master: Added 101-cocktail_sort_list C.F that Sorts integers using Cocktail sort algorithm.

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -27,4 +27,6 @@ void quick_sort(int *array, size_t size);
 void partition_caller(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap(listint_t **list, listint_t **node);
 #endif

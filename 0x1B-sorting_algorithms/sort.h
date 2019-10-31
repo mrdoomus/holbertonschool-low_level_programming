abb4f2878267a9adf8cdbc53f39aa3829218c793
mrdoomus <mrdoom.official@gmail.com>
2019-10-31 03:07:41 +0000

    master: Added 102-counting_sort & 103-merge_sort that sort and array with the counting and merging sorting algos respectively

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -29,4 +29,8 @@ int partition(int *array, int left, int right, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap(listint_t **list, listint_t **node);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge_caller(int *sub_array, int *array, int left, int right);
+void merge(int *sub_array, int *array, int left, int mid, int right);
 #endif

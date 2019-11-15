f7410f274bce21a5bb5f2873bbe33812b94f53f9
mrdoomus <mrdoom.official@gmail.com>
2019-11-15 14:06:27 -0500

    Uploaded every file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    '''Returns the perimeter of island in grid.'''
+
+    rows = (len(grid))
+    items = (len(grid[0]))
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(items):
+            if grid[i][j] == 1:
+                try:
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                    pass
+                try:
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                    pass
+                try:
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                    pass
+                try:
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                    pass
+    return(perimeter)

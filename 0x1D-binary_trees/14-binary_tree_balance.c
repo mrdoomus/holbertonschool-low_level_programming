38cb35d50e9e2c61f19e5fb7e3ca13c0c3afa01e
mrdoomus <mrdoom.official@gmail.com>
2019-12-04 14:30:43 -0500

    master: Added height function for balance recurssion

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,4 +1,27 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: height of the tree or 0 if NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+
+	if (left > right)
+		return (left);
+	return (right);
+}
+
 /**
  * binary_tree_balance - function that measures
  * the balance factor of a binary tree
@@ -12,14 +35,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (0);
-
 	if (tree->left)
-		left = 1 + binary_tree_balance(tree->left);
-
+		left = 1 + binary_tree_height(tree->left);
 	if (tree->right)
-		right = 1 + binary_tree_balance(tree->right);
+		right = 1 + binary_tree_height(tree->right);
 
 	return (left - right);
 }
